92e47056f6097178405f1e4f33971fb65b2cb0ca
dartSasant <sasantpudasaini@gmail.com>
2026-03-08 16:00:16 +0545

    Successfully created and fetched notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-import Navbar from "./components/Navbar";
-import RateLimitedUI from "./components/RateLimitUI";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitUI";
+import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -32,7 +33,20 @@ const HomePage = () => {
     <div>
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
-      
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">
+            Loading Notes........
+          </div>
+        )}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
